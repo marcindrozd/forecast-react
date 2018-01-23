33c272fcb7937cac659378a6c20472a2286d7580
Marcin Drozd <marcindrozd@me.com>
2018-01-23 16:34:17 +0100

    Clean up some comments

--- src/__tests__/components/CurrentLocationInfo.test.js
+++ src/__tests__/components/CurrentLocationInfo.test.js
@@ -5,19 +5,6 @@ const mockStore = configureStore();
 
 import CurrentLocationInfo from '../../components/CurrentLocationInfo';
 
-// const setup = () => {
-//   const props = {
-//     location: 'New York'
-//   };
-
-//   const enzymeWrapper = mount(<CurrentLocationInfo {...props} />);
-
-//   return {
-//     props,
-//     enzymeWrapper
-//   }
-// }
-
 describe('CurrentLocationInfo', () => {
   it('should render location', () => {
     const wrapper = mount(<CurrentLocationInfo store={mockStore({ location: { city: 'New York' } })} />);
